42a355a4dba26d414a0b08bcc30ef5650bed92b4
Petter Wahlman <pwahlman@cisco.com>
2017-07-02 11:29:24 +0200

    Revert "constness"
    This reverts commit 2b2bf3030f9fc54e1e9bdaa08d1b2a7541eb755c.

--- src/bat.cpp
+++ src/bat.cpp
@@ -1,17 +1,25 @@
-// bat -- combined cat, dd and hexdump
-
-#include <errno.h>
+/*
+ * (c) petter wahlman, petter@wahlman.no
+ *
+ * bat -- combined cat, dd and hexdump
+ *
+ */
 #include "bat.hpp"
 
+#include <errno.h>
+
 #ifdef _WIN32
-    #define fileno _fileno
-    bool isatty(int) { return true; }
-    void umask(int) { }
+bool isatty(int) {
+    return true;
+}
+void umask(int) { }
+# define fileno _fileno
 #endif
+
+#include <sys/stat.h>
 
 #include "boost/program_options.hpp"
 #include <iostream>
-#include <sys/stat.h>
 
 namespace po = boost::program_options;
 
@@ -75,7 +83,7 @@ FILE * open_dest_file(const std::string & filename, std::size_t offset, std::siz
             throw "unable to seek";
 
         return fp;
-    } catch (const char * what) {
+    } catch (std::string & what) {
         std::cout << "error, " << what << std::endl;
     }
     return nullptr;
@@ -99,7 +107,7 @@ void bat::print_colorized(const std::string & col) const
         fprintf(fpo_, "%s", col.c_str());
 }
 
-void bat::print_array() const
+void bat::print_array()
 {
     fprintf(fpo_, "    ");
     for (std::size_t j = 0; j < cfg.bytes_on_line; ++j) {
@@ -109,7 +117,7 @@ void bat::print_array() const
     }
 }
 
-void bat::print_hex() const
+void bat::print_hex()
 {
     cfg.colorize = true;
     std::size_t j = 0;
@@ -136,7 +144,7 @@ void bat::print_hex() const
     }
 }
 
-void bat::print_binary() const
+void bat::print_binary()
 {
     cfg.colorize = true;
     std::size_t j = 0;
@@ -163,7 +171,7 @@ void bat::print_binary() const
     }
 }
 
-void bat::print_words() const
+void bat::print_words()
 {
     // todo: swap word
     std::size_t j = 0;
@@ -192,7 +200,7 @@ void bat::print_words() const
     }
 }
 
-void bat::print_ascii() const
+void bat::print_ascii()
 {
     std::size_t j = 0;
     while (j < cfg.bytes_on_line) {
@@ -216,22 +224,20 @@ void bat::print_ascii() const
     }
 }
 
-void bat::formated_output() const
+// always starts on row 0:
+void bat::formated_output()
 {
-    auto bytes_on_line = cfg.bytes_on_line;
-    auto bytes_per_line = cfg.bytes_per_line;
-    auto relative_offset = 0;
-
-    while (relative_offset < quantum_.size()) {
-        if ((relative_offset + bytes_per_line) < quantum_.size())
-            bytes_on_line = bytes_per_line;
+    cfg.relative_offset = 0;
+    while (cfg.relative_offset < quantum_.size()) {
+        if ((cfg.relative_offset + cfg.bytes_per_line) < quantum_.size())
+            cfg.bytes_on_line = cfg.bytes_per_line;
         else
-            bytes_on_line = quantum_.size() - relative_offset;
+            cfg.bytes_on_line = quantum_.size() - cfg.relative_offset;
 
         if (cfg.print_flags & PRINT_ARRAY) {
             print_array();
             fprintf(fpo_, "\n");
-            relative_offset += bytes_on_line;
+            cfg.relative_offset += cfg.bytes_on_line;
             continue;
         }
 
@@ -270,7 +276,7 @@ void bat::formated_output() const
         else
             fputc('\n', fpo_);
 
-        relative_offset += bytes_on_line;
+        cfg.relative_offset += cfg.bytes_on_line;
     }
 }
 
--- src/bat.hpp
+++ src/bat.hpp
@@ -62,7 +62,7 @@ public:
     std::string source_file;
     std::string dest_file;
     unsigned print_flags;
-    mutable bool colorize;
+    bool colorize;
 };
 
 boost::optional<config> parse_args(int argc, char ** argv);
@@ -86,12 +86,12 @@ public:
                const std::size_t dest_offset);
 private:
     void print_colorized(const std::string & col) const;
-    void print_array() const;
-    void print_hex() const;
-    void print_binary() const;
-    void print_words() const;
-    void print_ascii() const;
-    void formated_output() const;
+    void print_array();
+    void print_hex();
+    void print_binary();
+    void print_words();
+    void print_ascii();
+    void formated_output();
 
     config cfg;
     FILE * fp_;
